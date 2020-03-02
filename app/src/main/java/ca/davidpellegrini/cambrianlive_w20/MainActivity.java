0b5f4edd7d3ce5be16058233b13c872345d94bf2
David Pellegrini <dap@davidpellegrini.com>
2020-03-01 19:19:46 -0500

    Prep Java code for class

--- app/src/main/java/ca/davidpellegrini/cambrianlive_w20/MainActivity.java
+++ app/src/main/java/ca/davidpellegrini/cambrianlive_w20/MainActivity.java
@@ -3,106 +3,44 @@ package ca.davidpellegrini.cambrianlive_w20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.TextView.OnEditorActionListener;
 import android.widget.Button;
-import android.view.KeyEvent;
-import android.view.inputmethod.EditorInfo;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 
 import java.text.NumberFormat;
 
-public class MainActivity extends AppCompatActivity
-implements OnEditorActionListener, OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private TextView tipAmountTextView, totalAmountTextView, tipPercentTextView;
     private EditText billAmountEditText;
     private Button percentUpButton, percentDownButton;
+    private CheckBox rememberTipCheckbox;
+    private RadioGroup splitBillRadioGroup;
+    private RadioButton onePerson, twoPeople, threePeople, fourPeople;
 
-    private String billAmountString = "";
-    private float tipPercent = 15f;
-
-    private SharedPreferences savedValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tipAmountTextView = (TextView) findViewById(R.id.TipAmountTextView);
-        totalAmountTextView = (TextView) findViewById(R.id.totalTextView);
+        tipAmountTextView = findViewById(R.id.TipAmountTextView);
+        totalAmountTextView = findViewById(R.id.totalTextView);
         tipPercentTextView = findViewById(R.id.TipPercentTextView);
-        billAmountEditText = (EditText) findViewById(R.id.BillAmountEditText);
-        billAmountEditText.setOnEditorActionListener(this);
+        billAmountEditText = findViewById(R.id.BillAmountEditText);
 
-        percentDownButton = (Button) findViewById(R.id.decreaseButton);
-        percentUpButton = (Button) findViewById(R.id.increaseButton);
-        percentUpButton.setOnClickListener(this);
-        percentDownButton.setOnClickListener(this);
+        percentDownButton = findViewById(R.id.decreaseButton);
+        percentUpButton = findViewById(R.id.increaseButton);
 
-        savedValues = getSharedPreferences("SavedValues", MODE_PRIVATE);
+        rememberTipCheckbox = findViewById(R.id.rememberTipCheckBox);
 
-    }
-
-    public void onPause() {
-        Editor editor = savedValues.edit();
-        editor.putString("billAmountString", billAmountString);
-        editor.putFloat("tipPercent", tipPercent);
-        editor.commit();
-
-        super.onPause();
-    }
-
-    public void onResume() {
-        super.onResume();
-
-        billAmountString = savedValues.getString("billAmountString", "");
-        tipPercent = savedValues.getFloat("tipPercent", 0.15f);
-        billAmountEditText.setText(billAmountString);
-        calculateAndDisplay();
-    }
-
-    public boolean onEditorAction(TextView v, int actionId, KeyEvent event){
-        if(actionId == EditorInfo.IME_ACTION_DONE ){
-            calculateAndDisplay();
-        }
-
-        return false;
-    }
-
-    public void calculateAndDisplay() {
-        billAmountString = billAmountEditText.getText().toString();
-        float billAmount;
-        if(billAmountString.equals("")){
-            billAmount = 0;
-        }
-        else{
-            billAmount = Float.parseFloat(billAmountString);
-        }
-        float tipAmount = billAmount * tipPercent;
-        float totalAmount = billAmount + tipAmount;
-
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        tipAmountTextView.setText(currency.format(tipAmount));
-        totalAmountTextView.setText(currency.format(totalAmount));
-
-        NumberFormat percent = NumberFormat.getPercentInstance();
-        tipPercentTextView.setText(percent.format(tipPercent));
-    }
-
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.decreaseButton:
-                tipPercent = tipPercent - 0.01f;
-                break;
-            case R.id.increaseButton:
-                tipPercent = tipPercent + 0.01f;
-                break;
-        }
-        calculateAndDisplay();
+        splitBillRadioGroup = findViewById(R.id.splitRadioGroup);
+        onePerson = findViewById(R.id.radioButton_1);
+        twoPeople = findViewById(R.id.radioButton_2);
+        threePeople = findViewById(R.id.radioButton_3);
+        fourPeople = findViewById(R.id.radioButton_4);
     }
 }
